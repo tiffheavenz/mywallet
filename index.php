ca576e0bd38d031a7405eac5840f9ad819a55e56
tiffheavenz <tiffheavenz@gmail.com>
2026-08-05 16:18:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $bots = [
     ],
 
     [
-        "token" => "8565074370:AAFz_Opi7kYiAJc5ptVHhsxNzIEPAZIYUpUE",
+        "token" => "8880567979:AAEh_kpBSs7YzAYqLO_G6ZUqF-6m0nQJmWs",
         "chat_id" => "8938414761"
     ]
 
